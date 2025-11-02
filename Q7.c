2fd012d16af0feeceae999bd7dffd5ec6869c9ab
Vrinda <yvrinda67@gmail.com>
2025-11-02 22:20:42 +0530

    Q7.c

---
+++ Q7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+  
+    int n = 5; // number of rows
+    int i, j, space;
+
+    for (i = 1; i <= n; i++) {
+        // Left binary sequence
+        for (j = 1; j <= i; j++) {
+            printf("%d", j % 2);   
+        }
+
+        
+        space = (n - i) * 4 - 2;   
+        if (space < 0)
+            space = 0;
+        for (j = 1; j <= space; j++) {
+            printf(" ");
+        }
+
+        // Right binary sequence
+        for (j = 1; j <= i; j++) {
+            printf("%d", j % 2);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
