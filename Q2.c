ddc9d6b1483d8c72c0a52f40e8424e7608b3f57a
Vrinda <yvrinda67@gmail.com>
2025-11-02 20:08:32 +0530

    Q2.c

---
+++ Q2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Recursive function to find HCF using Euclid’s algorithm
+int findHCF_recursive(int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return findHCF_recursive(b, a % b);
+}
+
+// Iterative method to find HCF
+int findHCF_iterative(int a, int b) {
+    int temp;
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Using recursive method
+    int hcf_rec = findHCF_recursive(num1, num2);
+    printf("\nHCF (using recursion) of %d and %d = %d\n", num1, num2, hcf_rec);
+
+    // Using iterative method
+    int hcf_itr = findHCF_iterative(num1, num2);
+    printf("HCF (using iteration) of %d and %d = %d\n", num1, num2, hcf_itr);
+
+    return 0;
+}
